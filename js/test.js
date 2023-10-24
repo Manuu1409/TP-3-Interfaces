a3a5166558b729b5d1da04184f16e0731eed7cb1
Manuu1409 <manuracingclub61@gmail.com>
2023-10-23 21:12:38 -0300

    ejemplo boton

--- js/test.js
+++ js/test.js
@@ -1,3 +1,4 @@
+/*
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let width = 500;
@@ -26,3 +27,57 @@ img_supermann.onload = function() {
         ctx.drawImage(img_supermann, Math.round(Math.random() * width) - 20, Math.round(Math.random() * height) - 20, 40, 40);
     }
 };
+
+*/
+
+
+let canvas2 = document.getElementById("canvas2");
+let ctx2 = canvas2.getContext('2d');
+let buttonDrawSuperman = document.getElementById('buttonDrawSuperman');
+let buttonDrawBatman = document.getElementById('buttonDrawBatman');
+
+let width2 = canvas2.width;
+let height2 = canvas2.height;
+
+let drawing = false;
+
+function getMousePos(canvas, evt){
+    let ClientRect = canvas.getBoundingClientRect();
+
+    return {
+        x: Math.round(evt.clientX - ClientRect.left),
+        y: Math.round(evt.clientY - ClientRect.top)
+    }
+}
+
+buttonDrawSuperman.addEventListener('click', function(){
+    drawing = !drawing;
+    this.innerHTML = (drawing) ? 'Parar' : 'Superman';
+    drawImage('images/supermann.png');
+});
+
+buttonDrawBatman.addEventListener('click', function(){
+    drawing = !drawing;
+    this.innerHTML = (drawing) ? 'Parar' : 'Batman';
+    drawImage('images/batmann.png');
+});
+
+function drawImage(src) {
+    ctx2.beginPath();
+    canvas2.addEventListener('click', function(evt){
+        if(drawing){
+            let m = getMousePos(canvas2, evt);
+            ctx2.lineWidth = 2;
+            ctx2.strokeStyle = '#A1C25E';
+
+            let img = new Image();
+            img.src = src;
+
+            img.onload = function() {
+                ctx2.drawImage(img, m.x - 25, m.y - 25, 50, 50);
+            };
+        }
+    }, false);
+}
+
+
